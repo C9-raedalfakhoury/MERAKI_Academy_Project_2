650d8e8ad45250c81ece1c199c282ee6d87fbeb5
raedalfakhoury <alfakhouryraed@gmail.com>
2023-11-25 01:27:51 +0300

    cart section 98%

--- main.js
+++ main.js
@@ -59,12 +59,35 @@ const products = [
     price: 20,
     Quantity: 1,
   },
+];const mobile = [
+  {
+    id: 1,
+    title: "about product1",
+    imageSrc: "./images/phone1.jpeg",
+    description:
+      "1 a wireless handheld device that allows users to make and receive calls.",
+    rate: "⭐⭐⭐",
+    price: 25,
+    Quantity: 1,
+  },
+  {
+    id: 2,
+    title: "about product2",
+    imageSrc: "./images/phone1.jpeg",
+    description:
+      "2 a wireless handheld device that allows users to make and receive calls.",
+    rate: "⭐⭐⭐⭐⭐",
+    price: 24,
+    Quantity: 1,
+  },
+
+ 
 ];
  
 const body = $("body");
 
 const btnMode = $("#mode");
-
+$("#Basket").html(`My Basket (${localStorage.getItem("totalQuantity") || 0})`)
 function toggle() {
   if (btnMode.val() === "DarkMode") {
     btnMode.val("LightMode");
@@ -229,22 +252,103 @@ const renderallproducts = () => {
     $(".arrowBack").css({
       display: "block",
     });
-
-    $("#Basket")[0].value = ` My Basket  (${totalQuantity}) `;
+    $("#Basket").html(` My Basket  (${totalQuantity}) `);
+   // $("#Basket")[0].value = ` My Basket  (${totalQuantity}) `;
   });
 
-  const myBasket = $(
-    `<input type="button" id="Basket" onclick="basketScreen()" value="My Basket (${
-      localStorage.getItem("totalQuantity") || 0
-    })">`
-  );
+  // const myBasket = $(
+  //   `<input type="button" id="Basket" onclick="basketScreen()" value="My Basket (${
+  //     localStorage.getItem("totalQuantity") || 0
+  //   })">`
+  // );
 
-  $(".baskesBin").append(myBasket);
+  // $(".baskesBin").append(myBasket);
+  $(".OK").on("click", close);
+};
+const renderMobile = () => {
+  mobile.forEach((elem, index) => {
+    const item = $(` <div id="${index + 1}"  class="cardbutton">
+    <div id="${index + 1}" class="card"  title="click for more details">
+      <img id="image" src="${elem.imageSrc}" />
+      <div>
+        <p>${elem.title}</p>
+      </div>
+      <div id="iconPrice">
+        <p class="price">$${elem.price}</p>
+        <div class="starIcon">
+        <h2>${elem.rate}</h2>
+        
+        </div>
+      </div>
+    </div>
+    <div id=${index + 1} class="addToCard1"><button id=${
+      index + 1
+    } class="addToCart" >Add To Cart</button></div>
+  </div>`);
+    $(".master2").append(item);
+  });
+  // !! for popup------------------------
+  $(".card").on("click", function open() {
+    $(".poph2").text(products[$(this)[0].id - 1].title);
+    $(".imgPop")[0].src = products[$(this)[0].id - 1].imageSrc;
+    $(".detail").text(products[$(this)[0].id - 1].description);
+    $(".popup").css({
+      visibility: "visible",
+      transform: "translate(-50%,-50%) scale(1)",
+      top: "50%",
+    });
+    $(".master").css({
+      opacity: "0.4",
+      "pointer-events": "none",
+    });
+  });
+  // !! -----------for addToCard -----------
+  $(".addToCard1").on("click", function () {
+    const xx = products.find((x) => x.id == $(this)[0].id);
+    if (myListProduct.includes(xx)) {
+      totalPrice.push(xx.price);
+      localStorage.setItem("totalPrice", totalPrice);
+      xx.Quantity++;
+      localStorage.setItem("myListProduct", JSON.stringify(myListProduct));
+    } else {
+      myListProduct.push(xx);
+
+      totalPrice.push(xx.price);
+      localStorage.setItem("myListProduct", JSON.stringify(myListProduct));
+      localStorage.setItem("totalPrice", totalPrice);
+    }
+    const finalQuantity = JSON.parse(
+      localStorage.getItem("myListProduct")
+    )?.map((ele) => {
+      return ele.Quantity;
+    });
+    const totalQuantity = finalQuantity?.reduce(
+      (accumulator, currentValue) => accumulator + currentValue,
+      0
+    );
+    localStorage.setItem("totalQuantity", totalQuantity);
+    console.log(totalQuantity);
+    $(".arrowBack").css({
+      display: "block",
+    });
+
+   // $("#Basket")[0].value = ` My Basket  (${totalQuantity}) `;
+   $("#Basket").html(`My Basket (${totalQuantity})`)
+  });
+
+  // const myBasket = $(
+  //   `<input type="button" id="Basket" onclick="basketScreen()" value="My Basket (${
+  //     localStorage.getItem("totalQuantity") || 0
+  //   })">`
+  // );
+
+  // $(".baskesBin").append(myBasket);
+  
   $(".OK").on("click", close);
 };
 // !! ----------finish render-------------
 
-function basketScreen() {
+const  basketScreen=()=> {
   $(".totalPrice").show();
   $(".headerBasket").css({
     display: "flex",
@@ -307,29 +411,35 @@ $(".bt").on("click", function () {
     // Remove the corresponding HTML element
     $(this).closest(".myProduct").remove();
 
-    // Recalculate total price
-    const finalPrice = JSON.parse(localStorage.getItem("myListProduct"))?.map(
-      (ele) => ele.price * ele.Quantity
-    );
+    // Check if there are no more items in the cart
+    if (myListProduct.length === 0) {
+      // Clear the entire local storage
+      localStorage.clear();
+    } else {
+      // Recalculate total price
+      const finalPrice = JSON.parse(localStorage.getItem("myListProduct"))?.map(
+        (ele) => ele.price * ele.Quantity
+      );
 
-    const sumWithInitial = finalPrice?.reduce(
-      (accumulator, currentValue) => accumulator + currentValue,
-      0
-    );
+      const sumWithInitial = finalPrice?.reduce(
+        (accumulator, currentValue) => accumulator + currentValue,
+        0
+      );
 
-    // Update the total price display
-    $(".totalPrice").text("Total Price" + " " + sumWithInitial);
+      // Update the total price display
+      $(".totalPrice").text("Total Price" + " " + sumWithInitial);
 
-    // Update the total quantity in the basket
-    const finalQuantity = JSON.parse(localStorage.getItem("myListProduct"))?.map(
-      (ele) => ele.Quantity
-    );
-    const totalQuantity = finalQuantity?.reduce(
-      (accumulator, currentValue) => accumulator + currentValue,
-      0
-    );
-    localStorage.setItem("totalQuantity", totalQuantity);
-    $("#Basket")[0].value = ` My Basket  (${totalQuantity}) `;
+      // Update the total quantity in the basket
+      const finalQuantity = JSON.parse(localStorage.getItem("myListProduct"))?.map(
+        (ele) => ele.Quantity
+      );
+      const totalQuantity = finalQuantity?.reduce(
+        (accumulator, currentValue) => accumulator + currentValue,
+        0
+      );
+      localStorage.setItem("totalQuantity", totalQuantity);
+      $("#Basket")[0].value = ` My Basket  (${totalQuantity}) `;
+    }
   }
 });
     $(".myCart").append(final);
@@ -340,6 +450,9 @@ $(".bt").on("click", function () {
   }
   $(".totalPrice").text("Total Price" + " " + sumWithInitial);
 }
+$("#Basket").on("click",function(){
+  basketScreen()
+})
 const hide = function () {
   $(".totalPrice").hide();
   $(".arrowBack").hide();
@@ -352,5 +465,24 @@ const hide = function () {
     display: "flex",
   });
 };
+renderallproducts(products) 
+renderMobile(mobile) 
 
-$(window).on("load", renderallproducts(products));
+$(".master2").hide()
+$("#mobile").on("click",function(){
+  $(".master").hide()
+  $(".master2").show()
+  
+
+})
+$("#shoes").on("click",function(){
+
+   $(".master2").hide()
+  $(".master").show()
+//render(products)
+ 
+
+ 
+})
+//  renderallproducts(products)
+// $(window).on("load",);
